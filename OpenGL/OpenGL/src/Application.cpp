07ac0518e84a6334f1f1a6fd2c36b7008231fd68
EnzoFerrari430 <782794844@qq.com>
2021-08-18 22:43:48 +0800

    代码添加glew库获取显卡opengl版本

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,13 +1,32 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+//#include <GL/glew.h>//#error:  gl.h included before glew.h  gl.h存在在 GLFW/glfw3.h 中  所以要将glew.h放在最上面
+
+#include <iostream>
+
 /*
 https://www.bilibili.com/video/BV1MJ411u7Bc
 
 
+02:
 legacy opengl:
 	早期的opengl使用立即渲染模式（固定渲染管线）  方便但不灵活
 modern opengl:
 	现代opengl使用核心模式（core-profile）  更好的灵活性和效率，但是更难学
 	
+
+GLFW:
+	是一个专门针对OpenGL的C语言库，允许用户创建OpenGL上下文，定义窗口参数以及处理用户的输入
+
+
+
+03:
+微软自带的gl库只支持到opengl1.1版本，要使用新的opengl，使用opengl的高级特性，就必须下载最新的扩展
+而且不同的显卡公司也会发布一些只有自家显卡才支持的扩展函数。所以手动去显卡驱动dll加载会显得很麻烦
+glew扩展库可以解决这些问题，因为GLEW能自动识别你的平台支持的全部OpenGL高级扩展函数
+glew:
+	OpenGL extension weangler
+
 */
 int main(void)
 {
@@ -16,6 +35,15 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
+	//需要预定义 GLEW_STATIC
+	//glewInit不能放在这里
+	//First you need to create a valid OpenGL rendering context and call glewInit() to initialize the extension entry points
+	//if (glewInit() != GLEW_OK)
+	//{
+	//	std::cout << "error" << std::endl;
+	//}
+
 
 	//创建一个窗口以及关联的opengl或opengl es环境(OpenGL or OpenGL ES context)
 	//大部分的控制窗口及其环境选项都是通过窗口提示（window hints）指定的
@@ -33,10 +61,19 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "error" << std::endl;
+	}
+
+	//获取现在OpenGL版本
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	//判断当前窗口的关闭标志是否为true
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		//微软实现的自带的gl库
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
