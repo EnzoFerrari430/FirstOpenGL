5cb72d7fa4c789f2262a7b426a114f06cd2c047e
EnzoFerrari430 <782794844@qq.com>
2024-01-24 09:52:55 +0800

    添加基础文件

---
+++ LearnOpenGL_CMake/includes/learnopengl/filesystem.h
@@ -0,0 +1,47 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+
+#include <string>
+#include "root_directory.h"
+
+class FileSystem
+{
+private:
+    typedef std::string(*Builder)(const std::string& path);
+
+public:
+    static std::string getPath(const std::string& path)
+    {
+        static std::string(*pathBuilder)(const std::string&) = getPathBuilder();
+        return (*pathBuilder)(path);
+    }
+
+private:
+    static const std::string& getRoot()
+    {
+        static char const* envRoot = getenv("LOGL_ROOT_PATH");
+        static char const* givenRoot = (envRoot != nullptr ? envRoot : logl_root);
+        static std::string root = (givenRoot != nullptr ? givenRoot : "");
+        return root;
+    }
+
+    static Builder getPathBuilder()
+    {
+        if (getRoot() != "")
+            return &FileSystem::getPathRelativeRoot;
+        else
+            return &FileSystem::getPathRelativeBinary;
+    }
+
+    static std::string getPathRelativeRoot(const std::string& path)
+    {
+        return getRoot() + std::string("/") + path;
+    }
+
+    static std::string getPathRelativeBinary(const std::string& path)
+    {
+        return "../../../" + path;
+    }
+};
+
+#endif
