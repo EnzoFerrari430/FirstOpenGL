ffd19e97bb5c8f7f47112e29255cf888ada1bab5
EnzoFerrari430 <782794844@qq.com>
2024-12-04 20:58:16 +0800

    片段着色器代码清理

--- LearnOpenGL_CMake/src/5.advanced_lighting/8.1.deferred_shading/8.1.g_buffer.fs
+++ LearnOpenGL_CMake/src/5.advanced_lighting/8.1.deferred_shading/8.1.g_buffer.fs
@@ -23,24 +23,3 @@ void main()
     // 镜面反射强度
     gAlbedoSpec.a = texture(texture_specular1, fs_in.TexCoords).r;
 }
-
-
-
-//#version 330 core
-//
-//out vec4 FragColor;
-//
-//in VS_OUT
-//{
-//    vec3 FragPos;
-//    vec3 Normal;
-//    vec2 TexCoords;
-//} fs_in;
-//
-//uniform sampler2D texture_diffuse1;
-//uniform sampler2D texture_specular1;
-//
-//void main()
-//{
-//    FragColor = texture(texture_diffuse1, fs_in.TexCoords);
-//}
